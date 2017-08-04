3c0e0b35781aab057cf3d9137f3538f79f2d6a1c
Mykola Fomin <giraffe.in.ua@gmail.com>
2017-08-04 18:39:42 +0300

    Call parseFeed method in NewsTableViewController. Parse feed and display data in cells.

--- SimpleRSSReader/NewsTableViewController.swift
+++ SimpleRSSReader/NewsTableViewController.swift
@@ -9,18 +9,23 @@
 import UIKit
 
 class NewsTableViewController: UITableViewController {
+    
+    private var rssItems: [(title: String, description: String, pubDate: String)]?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         tableView.estimatedRowHeight = 155.0
         tableView.rowHeight = UITableViewAutomaticDimension
+        
+        let feedParser = FeedParser()
+        feedParser.parseFeed(feedURL: "https://www.apple.com/main/rss/hotnews/hotnews.rss") { (rssItems) in
+            self.rssItems = rssItems
+            OperationQueue.main.addOperation({
+                self.tableView.reloadSections(IndexSet(integer: 0), with: .none)
+            })
+        }
 
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
     // MARK: - Table view data source
@@ -32,7 +37,36 @@ class NewsTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // Return the number of rows
-        return 0
+        guard let rssItems = rssItems else {
+            return 0
+        }
+        return rssItems.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! NewsTableViewCell
+        
+        //Configure Cell
+        if let item = rssItems?[indexPath.row] {
+            cell.titleLabel.text = item.title
+            cell.descriptionLabel.text = item.description
+            cell.dateLabel.text = item.pubDate
+        }
+        return cell
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
